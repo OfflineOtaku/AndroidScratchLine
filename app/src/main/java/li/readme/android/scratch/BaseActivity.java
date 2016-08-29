d744b504521aa7b18cab7b11455a35b9966db1d4
lizhi <liz@copyleft.me>
2016-08-30 00:05:10 +0800

    添加测试依赖

--- app/src/main/java/li/readme/android/scratch/BaseActivity.java
+++ app/src/main/java/li/readme/android/scratch/BaseActivity.java
@@ -1,6 +1,7 @@
 package li.readme.android.scratch;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
@@ -49,5 +50,15 @@ public class BaseActivity extends AppCompatActivity {
   @Override
   public void finish() {
     super.finish();
+  }
+
+  @Override
+  public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+    super.onSaveInstanceState(outState, outPersistentState);
+  }
+
+  @Override
+  protected void onRestoreInstanceState(Bundle savedInstanceState) {
+    super.onRestoreInstanceState(savedInstanceState);
   }
 }
---
+++ app/src/test/java/li/readme/android/scratch/MainActivityTest.java
@@ -0,0 +1,21 @@
+package li.readme.android.scratch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+
+/**
+ * Created by August on 8/29/16.
+ */
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21)
+public class MainActivityTest {
+  @Test
+  public void testMainActivity() {
+    MainActivity mainActivity = Robolectric.setupActivity(MainActivity.class);
+    mainActivity.findViewById(R.id.finishOnResume).performClick();
+  }
+}
